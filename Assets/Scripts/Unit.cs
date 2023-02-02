6271982defe19422d7d85785f883fb6150bbfddd
SaintFils <70489331+SaintFils@users.noreply.github.com>
2023-02-02 20:16:52 +0400

    Animation transition adjustment, unit rotation to move direction

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 4.0f;
+    [SerializeField] private float rotationSpeed = 10.0f;
+    [SerializeField] private Animator unitAnimator;
     
     private Vector3 targetPosition;
 
@@ -16,6 +18,13 @@ public class Unit : MonoBehaviour
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             transform.position += moveDirection * (moveSpeed * Time.deltaTime);
+
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("IsWalking", false);
         }
 
         if (Input.GetMouseButtonDown(0))
