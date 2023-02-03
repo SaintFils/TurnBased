1e3168c85d435fb3c64ed929de4b3adfe03537aa
SaintFils <70489331+SaintFils@users.noreply.github.com>
2023-02-03 14:52:24 +0400

    Unit selection logic

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -11,6 +11,11 @@ public class Unit : MonoBehaviour
     
     private Vector3 targetPosition;
 
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
+
     private void Update()
     {
         float stoppingDistance = .1f;
@@ -26,14 +31,9 @@ public class Unit : MonoBehaviour
         {
             unitAnimator.SetBool("IsWalking", false);
         }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Move(MouseWorld.GetPosition());
-        }
     }
 
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
