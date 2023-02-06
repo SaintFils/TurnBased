1a34dcdb7d1bbfbac795f92efa3ff42493306f19
SaintFils <70489331+SaintFils@users.noreply.github.com>
2023-02-06 22:08:38 +0400

    LevelGrid added. Main logic from testing script was moved to LevelGrid.

--- Assets/Scripts/Grid/GridObject.cs
+++ Assets/Scripts/Grid/GridObject.cs
@@ -1,19 +1,43 @@
+using System.Collections.Generic;
+
 namespace Grid
 {
     public class GridObject
     {
         private GridPosition gridPosition;
         private GridSystem gridSystem;
+        private List<Unit> unitList;
+
+        public List<Unit> UnitList => unitList;
 
         public GridObject(GridSystem system, GridPosition position)
         {
             this.gridSystem = system;
             this.gridPosition = position;
+
+            unitList = new List<Unit>();
         }
 
         public override string ToString()
         {
-            return gridPosition.ToString();
+            string unitString = "";
+            
+            foreach (Unit unit in unitList)
+            {
+                unitString += unit + "\n";
+            }
+            
+            return gridPosition.ToString() + "\n" + unitString;
+        }
+
+        public void AddUnit(Unit unit)
+        {
+            unitList.Add(unit);
+        }
+
+        public void RemoveUnit(Unit unit)
+        {
+            unitList.Remove(unit);
         }
     }
 }
--- Assets/Scripts/Grid/GridPosition.cs
+++ Assets/Scripts/Grid/GridPosition.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace Grid
 {
-    public struct GridPosition
+    public struct GridPosition : IEquatable<GridPosition>
     {
         public int x;
         public int z;
@@ -14,6 +16,33 @@ namespace Grid
         public override string ToString()
         {
             return $"x: {x}; z: {z};";
+        }
+
+        public static bool operator ==(GridPosition a, GridPosition b)
+        {
+            return a.x == b.x && a.z == b.z;
+        }
+
+        public static bool operator !=(GridPosition a, GridPosition b)
+        {
+            return !(a == b);
+        }
+
+        public bool Equals(GridPosition other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is GridPosition position &&
+                   x == position.x &&
+                   z == position.z;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(x, z);
         }
     }
 }
